80c6fb1ad9c13662a96f90c1368faa3fc86ff779
Joshua Rizek <Joshua Rizek>
2020-08-27 19:32:15 -0400

    added accountInfo component

---
+++ src/pages/AccountInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class AccountInfo extends Component {
+    state = {
+        company:"",
+        job_title: "",
+        userImg:""
+    }
+    render() {
+        return (
+         <View>
+             
+         </View>
+        ) 
+    }
+}
